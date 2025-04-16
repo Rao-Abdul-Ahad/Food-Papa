56d8f58476bb04aa15a7339a41e94407f46d9b2f
Rao-Abdul-Ahad <abdulahad16191@gmail.com>
2025-04-16 12:22:41 +0500

    Add files via upload

---
+++ google.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBpMFsbbB-P-PXWAGaChiHIXoDh8LiRC9w",
+  authDomain: "sign-in-and-sign-up-335b4.firebaseapp.com",
+  projectId: "sign-in-and-sign-up-335b4",
+  storageBucket: "sign-in-and-sign-up-335b4.firebasestorage.app",
+  messagingSenderId: "832961840852",
+  appId: "1:832961840852:web:6d963abfb8f9de9fef5d41",
+  measurementId: "G-XVVF0TPN0Y"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.languageCode = 'en';
+const provider = new GoogleAuthProvider();
+let getbtn = document.getElementById("googlebtn");
+getbtn.addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const user = result.user;
+            console.log(user);
+            window.location.href = "adminpanel.html";
+        })
+
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            
+        });
+})
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    console.log(uid);
+    location.href = "adminpanel.html";
+    
+  } else {
+    console.log('User is signed out');
+    location.href = "index.html";
+    
+  }
+});
